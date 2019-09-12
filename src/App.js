208ceb6eef8232b5b4b0a53472a1384bd2c941d2
pqmcgill <pqmcgill@gmail.com>
2019-09-12 14:57:01 -0400

    removed cruft

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
 import React, { useEffect } from 'react';
-import fs from 'fs';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  useEffect(() => {
-    console.log('dirname', __dirname)
-    // example using native node dependencies in CRA
-    fs.readdir('./', (err, files) => {
-      files.forEach(file => console.log('file: ', file))
-    })
-  }, [])
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
